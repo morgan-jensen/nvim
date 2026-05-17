7eed25f559c401b979787de05235b5bdd35d6ca8
Morgan Jensen <mbjensen822@gmail.com>
2026-05-17 12:52:44 -0600

    updating opt calls to be new format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,33 +1,31 @@
-local o = vim.opt
-
 -- Interface
-o.number = true
-o.relativenumber = true
-o.cursorline = true
-o.wrap = false
-o.scrolloff = 10 -- Keep 10 lines above or below cursor
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.wrap = false
+vim.o.scrolloff = 10 -- Keep 10 lines above or below cursor
 
 -- Indentation
-o.tabstop = 4	-- Tab width
-o.shiftwidth = 4	-- Indent width
-o.smartindent = true
-o.autoindent = true
+vim.o.tabstop = 4	-- Tab width
+vim.o.shiftwidth = 4	-- Indent width
+vim.o.smartindent = true
+vim.o.autoindent = true
 
 -- Search settings
-o.ignorecase = true
-o.smartcase = true	-- Case sensitive if uppercase in search
-o.incsearch = true	-- Show matches as you type
+vim.o.ignorecase = true
+vim.o.smartcase = true	-- Case sensitive if uppercase in search
+vim.o.incsearch = true	-- Show matches as you type
 
 -- Visual settings
-o.termguicolors = true	-- Enable 24-bit colors
-o.signcolumn = "yes"	-- Always show sign column (column to the left of numbers for debug)
-o.showmatch = true	-- Show matching brackets
+vim.o.termguicolors = true	-- Enable 24-bit colors
+vim.o.signcolumn = "yes"	-- Always show sign column (column to the left of numbers for debug)
+vim.o.showmatch = true	-- Show matching brackets
 
 -- File handling
-o.undofile = true	-- Persistant undo
-o.undodir = vim.fn.expand("~/.vim/undodir") 	-- Undo directory
-o.autoread = true	-- Auto update files updated outside nvim
-o.autowrite = true	-- Auto save
+vim.o.undofile = true	-- Persistant undo
+vim.o.undodir = vim.fn.expand("~/.vim/undodir") 	-- Undo directory
+vim.o.autoread = true	-- Auto update files updated outside nvim
+vim.o.autowrite = true	-- Auto save
 
 -- Behavior settings
-o.encoding = "UTF-8"
+vim.o.encoding = "UTF-8"
